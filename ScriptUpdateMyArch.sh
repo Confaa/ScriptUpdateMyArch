6d6bbf271a4205fd3f298db523930000a0e3a52b
Confaa <juanconfaa@gmail.com>
2021-09-14 17:40:53 -0300

    Lista de dependencias levantadas por pacman & actualizado recursivo

--- ScriptUpdateMyArch.sh
+++ ScriptUpdateMyArch.sh
@@ -1,27 +1,29 @@
+#!/bin/bash
+
 echo "Actualizando sistema"
-sudo pacman -Syyu --noconfirm
-#Birdtray
-cd ~/AUR/birdtray/
-aux=$(git pull 2>&1)
-[[ "$aux" == "Already up to date." ]] && echo "Birdtray actualizada" || makepkg -si
-##########
-#Gitkraken
-cd ~/AUR/gitkraken/
-aux=$(git pull 2>&1)
-[[ "$aux" == "Already up to date." ]] && echo "GitKraken actualizada" || makepkg -si
-##########
-#Visual Paradigm Community
-cd ~/AUR/visual-paradigm-community/
-aux=$(git pull 2>&1)
-[[ "$aux" == "Already up to date." ]] && echo "Visual Paradigm Community actualizada" || makepkg -si
-##########
-#Visual Studio Code
-cd ~/AUR/visual-studio-code-bin/
-aux=$(git pull 2>&1)
-[[ "$aux" == "Already up to date." ]] && echo "Visual Studio Code actualizada" || makepkg -si
-##########
-#YouTube Music Desktop App
-cd ~/AUR/ytmdesktop/
-aux=$(git pull 2>&1)
-[[ "$aux" == "Already up to date." ]] && echo "YouTube Music Desktop App actualizada" || makepkg -si
-##########
+echo ""
+sudo pacman -Syu --noconfirm
+
+pacman -Qqm > listaAur.txt
+repoScript=$(pwd)
+
+while read dependencia
+do
+    if [ -d ~/AUR/$dependencia/ ]
+    then
+        cd ~/AUR/$dependencia/
+        aux=$(git pull 2>&1)
+        [[ "$aux" == "Already up to date." ]] && echo "$dependencia actualizada" || makepkg -si --noconfirm
+    else
+        echo "El directorio $dependencia no existe"
+        cd ~/AUR/
+        git clone https://aur.archlinux.org/$dependencia.git
+        cd $dependencia
+        makepkg -si --noconfirm
+    fi
+    rm *.gz *.zst *.rpm 2> /dev/null
+done < listaAur.txt
+
+cd $repoScript
+rm listaAur.txt
+
