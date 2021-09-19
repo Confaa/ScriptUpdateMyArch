56271189d69624a3bb9655f0cf828546be987829
Confaa <juanconfaa@gmail.com>
2021-09-19 15:31:45 -0300

    Pequeños retoques en comandos cd

--- ScriptUpdateMyArch.sh
+++ ScriptUpdateMyArch.sh
@@ -9,21 +9,21 @@ repoScript=$(pwd)
 
 while read dependencia
 do
-    if [ -d ~/AUR/$dependencia/ ]
+    if [ -d ~/AUR/"$dependencia"/ ]
     then
-        cd ~/AUR/$dependencia/
+        cd ~/AUR/"$dependencia"/ || exit
         aux=$(git pull 2>&1)
         [[ "$aux" == "Already up to date." ]] && echo "$dependencia actualizada" || makepkg -si --noconfirm
     else
         echo "El directorio $dependencia no existe"
-        cd ~/AUR/
+        cd ~/AUR/ || exit
         git clone https://aur.archlinux.org/$dependencia.git
-        cd $dependencia
+        cd "$dependencia" || exit
         makepkg -si --noconfirm
     fi
     rm *.gz *.zst *.rpm 2> /dev/null
 done < listaAur.txt
 
-cd $repoScript
+cd "$repoScript" || exit
 rm listaAur.txt
 
